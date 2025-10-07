99f72ff5043b37d30141ba51f90feb016f2aea35
Mojolaoluwa Gafar <Mojolaoluwa1212@gmail.com>
2025-10-07 16:46:20 +0100

    development payment flow

--- src/Components/EventDetailsPageComponents/SelectTicketModal.jsx
+++ src/Components/EventDetailsPageComponents/SelectTicketModal.jsx
@@ -219,7 +219,7 @@ const handlePayment = async () => {
     }
     console.log("Paid event — initiating Paystack payment...");
 
-    const response = await fetch("http://localhost:5000/api/payments/initiate", {
+    const response = await fetch("https://eventsmb-server.onrender.com/api/payments/initiate", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(paymentData),
